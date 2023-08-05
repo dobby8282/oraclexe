a085fc060065da4a7d2d6cf659d80a4a51899b3e
dobby8282 <dobbyisfree8282@gmail.com>
2023-08-05 15:52:48 +0900

    Update ex05-function.sql

--- ex05-function.sql
+++ ex05-function.sql
@@ -56,6 +56,76 @@ SELECT TRIM('H' FROM 'HelloWorld') FROM dual;
 SELECT TRIM(' ' FROM ' HelloWorld ') FROM dual;
 
 -- 숫자 함수
+-- ROUND() - 지정된 소수점 자릿수로 값을 반올림합니다.
+SELECT ROUND(45.926, 2) FROM dual;
+
+-- TRUNC() - 지정된 소수점 자릿수로 값을 전달합니다.
+SELECT TRUNC(45.926, 2) FROM dual;
+
+-- MOD() - 나눈 나머지를 반환합니다.
+SELECT MOD(1600, 300) FROM dual;
+
+-- CEIL() - 주어진 숫자를 올림하여 정수로 반환합니다.
+SELECT CEIL(45.2) FROM dual;
+
+
+-- 날짜 함수
+
+-- SYSDATE - 현재 날짜와 시간을 얻을수 있는 pseudo-column 
+SELECT SYSDATE FROM dual;
+
+/*
+날짜 연산
+    날짜에 숫자를 더하거나 빼서 결과 날짜 값을 구합니다.
+    두 날짜 사이의 일수를 알아내기 위해 빼기연산을 합니다.
+*/
+SELECT last_name, (sysdate - hire_date) / 7 AS WEEKS
+FROM  employees
+WHERE department_id = 90;
+
+/*
+날짜 조작 함수
+    MONTHS_BETWEEN(date1, date2) : 두 날짜 간의 월 수를 계산합니다.
+    ADD_MONTHS(date, n) : 날짜에 n개월을 추가합니다.
+    NEXT_DAY(date, day_of_week) : 지정된 날짜의 다음으로 주어진 요일이 나오는 날짜를 계산합니다. 
+                                 (1:일요일 ~ 7:토요일)
+    LAST_DAY(date) : 주어진 월의 마지막 날짜를 반환합니다.
+    ROUND(date, format) : 날짜를 지정된 형식으로 반올림합니다.
+    TRUNC(date, format) : 날짜를 지정된 형식으로 절삭합니다.
+*/
+SELECT MONTHS_BETWEEN(TO_DATE('2017-12-22', 'YYYY-MM-DD'), TO_DATE('2017-05-22', 'YYYY-MM-DD')) FROM dual;
+SELECT ADD_MONTHS(TO_DATE('2022-12-16', 'YYYY-MM-DD'), 1) FROM dual;
+SELECT NEXT_DAY(TO_DATE('2023-08-05', 'YYYY-MM-DD'), 7) FROM dual;
+SELECT LAST_DAY(SYSDATE) FROM dual;
+SELECT ROUND(TO_DATE('2023-08-16', 'YYYY-MM-DD'), 'MONTH') FROM dual;
+SELECT TRUNC(SYSDATE, 'MONTH') FROM dual;
+
+-- 변환 함수
+/*
+TO_CHAR() 함수 - 날짜 또는 숫자를 문자열로 변환
+
+    YYYY - 전체 연도를 숫자로 나타냅니다.
+    YEAR - 영어 철자로 표기된 연도를 반환합니다.
+    MM - 월의 2자리 숫자 값을 반환합니다.
+    MONTH - 전체 월 이름을 반환합니다.
+    MON - 월의 3자리 약어를 반환합니다.
+    DY - 3자리 요일 약어를 반환합니다.
+    DAY - 요일의 전체 이름을 반환합니다.
+    DD - 월간 일(1-31)을 숫자 형식으로 반환합니다.
+    HH, HH12, HH24 - 1일동안 시간 또는 반일 시간(1-12) 또는 전일 시간(0-23)을 반환합니다.
+    MI - 분(0-59)을 반환합니다.
+    SS - 초(0-59)를 반환합니다.
+    FF - 밀리세컨드(0-999)를 반환합니다.
+    AM 또는 PM - 오전/오후를 나타내는 자오선 표시 반환합니다.
+    A.M. 또는 P.M. - 오전/오후를 나타내는 마침표가 있는 자오선 표시를 반환합니다.
+  
+*/
+SELECT last_name, TO_CHAR(hire_date, 'YYYY/MM/DD HH24:MI:SS') AS HIREDATE
+FROM employees;
+
+
+
+
 
 
 
