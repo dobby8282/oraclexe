bfb56f917694bf0861296c88568d724b23524157
dobby8282 <dobbyisfree8282@gmail.com>
2023-08-06 16:53:49 +0900

    Update ex07-select-join.sql

--- ex07-select-join.sql
+++ ex07-select-join.sql
@@ -72,8 +72,8 @@ ON worker.manager_id = manager.employee_id;
 /*
 Nonequijoin
     등호 연산자 외의 다른 연산자를 포함하는 조인 조건입니다.
-*/
-
+    
+    
 CREATE TABLE job_grades (
 grade_level CHAR(1),
 lowest_sal NUMBER(8,2) NOT NULL,
@@ -91,9 +91,76 @@ INSERT INTO job_grades VALUES ('E', 15000, 24999);
 INSERT INTO job_grades VALUES ('F', 25000, 40000);
 
 COMMIT;
+*/
+SELECT e.last_name, e.salary, j.grade_level
+FROM employees e JOIN job_grades j
+ON e.salary BETWEEN j.lowest_sal AND j.highest_sal;
+
+/*
+INNER JOIN과 OUTER JOIN
+
+    INNER JOIN 
+        일치하지 않는 행은 출력에 표시되지 않습니다. (교집합 해당 행 출력)
+        
+    OUTER JOIN
+        한 테이블의 행을 기반으로 다른 테이블과의 연결이 없는 행까지 포함하여 반환합니다.
+
+*/
+
+/*
+LEFT OUTER JOIN
+    DEPARTMENTS 테이블에 대응되는 행이 없어도
+    왼쪽 테이블인 EMPLOYEES 테이블의 모든 행을 검색합니다.
+*/
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e 
+LEFT OUTER JOIN departments d
+ON e.department_id = d.department_id;
 
 
-SELECT * FROM job_grades;
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e, departments d
+WHERE e.department_id = d.department_id(+);
+
+/*
+RIGHT OUTER JOIN
+    EMPLOYEES 테이블에 대응되는 행이 없어도
+    오른쪽 테이블인 DEPARTMENTS 테이블의 모든 행을 검색합니다.
+*/
+SELECT e.last_name, d.department_id, d.department_name
+FROM employees e 
+RIGHT OUTER JOIN departments d
+ON e.department_id = d.department_id;
+
+/*
+FULL OUTER JOIN
+    DEPARTMENTS, EMPLOYEES 대응되는 행이 없어도
+    테이블의 모든행을 검색합니다.
+*/
+SELECT e.last_name, d.department_id, d.department_name
+FROM employees e 
+FULL OUTER JOIN departments d
+ON e.department_id = d.department_id;
+
+/*
+Cartesian Product 
+    조인 조건이 잘못되거나 완전히 생략된 경우 결과가 모든 행의 조합이 표시되는 
+    Cartesian Product로 나타냅니다.
+*/
+
+/*
+CROSS JOIN
+    두 테이블의 교차 곱을 생성합니다.
+*/
+SELECT last_name, department_name
+FROM employees
+CROSS JOIN departments;
+
+
+
+
+
+
 
 
 
